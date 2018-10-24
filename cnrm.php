53a3efeb8f4081d18d11dc826cb20c27f884b83e
Yogaraj Khanal <yrkhanal@gmail.com>
2018-10-24 17:19:51 -0400

    Update cnrm.php

--- cnrm.php
+++ cnrm.php
@@ -7,6 +7,19 @@
 
 <?php
 echo "By Yograraj Khanal on PHP version " . phpversion();
+echo "<br />";
+
+   $host        = "host = hostname";
+   $port        = "port = 5432";
+   $dbname      = "dbname = dbname";
+   $credentials = "user =username  password=s3cr3T";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "PostgreSQL database connected successfully\n";
+   }
 ?>
 </body>
 </html>

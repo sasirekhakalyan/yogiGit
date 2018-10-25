b7fa669c2bb71eed11756a9abd320d3380dc1076
Yogaraj Khanal <yrkhanal@gmail.com>
2018-10-25 16:04:49 -0400

    Update cnrm.php

--- cnrm.php
+++ cnrm.php
@@ -5,8 +5,56 @@
 <title>Referral Program Elements</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style>
-li {
-list-style: none;
+
+
+input[type=text]:focus {
+    background-color: lightblue;
+}
+
+select {
+    width: 100%;
+    padding: 16px 20px;
+    border: none;
+    border-radius: 4px;
+    background-color: #f1f1f1;
+}
+input[type=button], input[type=submit], input[type=reset] {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-decoration: none;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+div#mainContiner{
+ background-color: #f2f2f2;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 100px; 
+}
+
+.select-style {
+    border: 1px solid #ccc;
+    width: 120px;
+    border-radius: 3px;
+    overflow: hidden;
+    background: #fafafa url("img/icon-select.png") no-repeat 90% 50%;
+}
+
+.select-style select {
+    padding: 5px 8px;
+    width: 130%;
+    border: none;
+    box-shadow: none;
+    background: transparent;
+    background-image: none;
+    -webkit-appearance: none;
+}
+
+.select-style select:focus {
+    outline: none;
 }
 
 </style>
@@ -18,41 +66,43 @@ list-style: none;
 <h1><center>Referral Program Elements</center></h1>
 
 <form name="insert" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
+<div style="overflow-x:auto;" id="mainContiner">
 <table style="width:80%">
 <tr>
-<td>First Name:</td><td><input type="text" name="first_name" value="<?php echo $first_name;?>"/></td>
-<td>Middle Name:</td><td><input type="text" name="middle_name" value="<?php echo $middle_name;?>" /></td>
-<td>Last Name:</td><td><input type="text" name="last_name" value="<?php echo $middle_name;?>"/></td>
+<td>First Name:</td><td><input type="text" name="first_name" required/>  </span></td>
+<td>Middle Name:</td><td><input type="text" name="middle_name" required /></td>
+<td>Last Name:</td><td><input type="text" name="last_name" required /></td>
 </tr>
 <tr>
 <td>Address:</td><td><input type="text" name="address" /></td>
 </tr>
 <tr>
-<td>City:</td><td><input type="text" name="city" /></td>
-<td>State:</td><td><input type="text" name="state" /></td>
-<td>Zip Code:</td><td><input type="text" name="zip_code" /></td>
+<td>City:</td><td><input type="text" name="city" required/></td>
+<td>State:</td><td><input type="text" name="state" required/></td>
+<td>Zip Code:</td><td><input type="text" name="zip_code" required/></td>
 </tr>
 
 <tr>
-<td>Phone No:</td><td><input type="text" name="phone_number" /></td>
-<td>Alternate Phone No:</td><td><input type="text" name="alternate_phone_number" /></td>
+<td>Phone No:</td><td><input type="text" name="phone_number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/></td>
+<td>Alternate Phone No:</td><td><input type="text" name="alternate_phone_number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/></td>
 </tr>
 
 <tr>
-<td>Email Address:</td><td><input type="text" name="email_address" /></td>
-<td>Age:</td><td><input type="text" name="age" /></td>
-<td>Injury Year:</td><td><input type="text" name="injury_year" /></td>
+<td>Email Address:</td><td><input type="text" name="email_address" pattern="^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$" required/></td>
+<td>Age:</td><td><input type="number" size="6"  min="18" max="85" name="age" required/></td>
+<td>Injury Year:</td><td><input type="number" size="8"  min="1900" max="2200" name="injury_year" required/></td>
 </tr>
 
 <tr>
 <td>TBI diagnosis:</td>
 <td>
-<input type="radio" name="tbi_diagnosis" value="true">Yes
+<input type="radio" name="tbi_diagnosis" value="true" required>Yes
 <input type="radio" name="tbi_diagnosis" value="false">No
 
 </td>
 <td>Military Status:</td><td>
-<select>
+<div class="select-style">
+<select required>
   <option value="military_status">Active Duty</option>
   <option value="military_status">Reservist</option>
   <option value="military_status">Guardsman</option>
@@ -60,6 +110,7 @@ list-style: none;
     <option value="military_status">Veteran</option>
     <option value="military_status">Unknown</option>
 </select>
+</div>
 </td>
 </tr>
 
@@ -67,7 +118,7 @@ list-style: none;
 <tr>
 <td>TBI Symptoms:</td>
 <td>
-<input type="radio" name="tbi_symptoms" value="true">Yes
+<input type="radio" name="tbi_symptoms" value="true" required>Yes
 <input type="radio" name="tbi_symptoms" value="false">No
 
 </td>
@@ -77,7 +128,7 @@ list-style: none;
 
 <tr>
 <td>Secondary Diagnosis PTS:</td><td>
-<input type="radio" name="secondary_diagnosis_pts" value="true">Yes
+<input type="radio" name="secondary_diagnosis_pts" value="true" required>Yes
 <input type="radio" name="secondary_diagnosis_pts" value="false">No
 </td>
 <td>What Symptoms of TBI are  most important symptoms to find treatment for?</td>
@@ -94,7 +145,7 @@ list-style: none;
 
 <tr>
 <td>Prior MRI:</td><td>
-<input type="radio" name="prior_mri" value="true">Yes
+<input type="radio" name="prior_mri" value="true" required>Yes
 <input type="radio" name="prior_mri" value="false">No
 </td>
 <td>
@@ -108,7 +159,7 @@ list-style: none;
 
 <tr>
 <td>Sharpnel:</td><td>
-<input type="radio" name="sharpnel" value="true">Yes
+<input type="radio" name="sharpnel" value="true" required>Yes
 <input type="radio" name="sharpnel" value="false">No
 </td>
 <td>
@@ -123,7 +174,7 @@ list-style: none;
 <tr>
 <td>DEERS Eligible:</td>
 <td>
-<input type="radio" name="deers_eligible" value="true">Yes
+<input type="radio" name="deers_eligible" value="true" required>Yes
 <input type="radio" name="deers_eligible" value="false">No
 </td>
 
@@ -138,7 +189,7 @@ list-style: none;
 
 <tr>
 <td>Enrolled in other CNRM studies?</td><td>
-<input type="radio" name="other_cnrm_studies_enrollment" value="true">Yes
+<input type="radio" name="other_cnrm_studies_enrollment" value="true" required>Yes
 <input type="radio" name="other_cnrm_studies_enrollment" value="false">No
 </td>
 <td>If Yes, Study Name</td><td><input type="text" name="other_enrolled_study_name" /></td>
@@ -147,12 +198,14 @@ list-style: none;
 <tr>
 <td>How Did you hear about us</td>
 <td>
-<select>
+<div class="select-style">
+<select required>
   <option value="information_source">Internet</option>
   <option value="information_source">CNN</option>
   <option value="information_source">Friends</option>
   <option value="information_source">NIH Seminar</option>
 </select>
+</div>
 </td>
 </tr>
 
@@ -165,7 +218,7 @@ list-style: none;
 </tr>
 
 </table>
-
+</div>
 
 
 
@@ -223,8 +276,8 @@ echo "<br />";
 
    $host        = "host = localhost";
    $port        = "port = 5432";
-   $dbname      = "dbname = cn";
-   $credentials = "user =user  password=pass";
+   $dbname      = "dbname = cnrm_data";
+   $credentials = "user =cnrm_user  password=pass";
 
 
 	

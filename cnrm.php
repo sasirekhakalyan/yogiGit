bf4757d54addeef420f2ffd1189e33a38db4fac4
Khanal <khanaly@ndsl-scss-2270.fios-router.home>
2018-11-28 16:45:07 -0500

    externalied db source file

--- cnrm.php
+++ cnrm.php
@@ -272,21 +272,7 @@ function test_input($data) {
 echo "By Yograraj Khanal on PHP version " . phpversion();
 echo "<br />";
 
-   $host        = "host = proforms-stage-db.cit.nih.gov";
-   $port        = "port = 5432";
-   $dbname      = "dbname = cnrm_data";
-   $credentials = "user =proforms_app_stg  password=Qhwq0iFZkKAc1NaU";
-
-
-	
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "PostgreSQL database connected successfully\n";
-   }
-
+include("db_config.php");
 
 echo "<br />";
 

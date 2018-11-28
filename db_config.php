c3af30bd4e9497e3613e3b5ad09eab1d902b10e1
Khanal <khanaly@ndsl-scss-2270.fios-router.home>
2018-11-28 16:39:14 -0500

    externalied db source file

---
+++ db_config.php
@@ -0,0 +1,14 @@
+<?php
+
+   $host        = "host = proforms-stage-db.cit.nih.gov";
+   $port        = "port = 5432";
+   $dbname      = "dbname = cnrm_data";
+   $credentials = "user =proforms_app_stg  password=Qhwq0iFZkKAc1NaU";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "PostgreSQL database connected successfully\n";
+   }
+?>

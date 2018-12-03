ce9982d64ed1a7f1e892e248e45284ba90b92578
khanaly <khanaly@ndsl-scss-2270.fios-router.home>
2018-12-03 17:07:40 -0500

    fix the logic to either allow your to login and enter data or redirect to register page.

--- pg_connection.php
+++ pg_connection.php
@@ -1,14 +1,7 @@
 <?php
-
    $host        = "host = proforms-stage-db.cit.nih.gov";
    $port        = "port = 5432";
    $dbname      = "dbname = cnrm_data";
    $credentials = "user =proforms_app_stg  password=Qhwq0iFZkKAc1NaU";
-
    $db = pg_connect( "$host $port $dbname $credentials"  ) or die('failed');;
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "PostgreSQL database connected successfully\n";
-   }
 ?>

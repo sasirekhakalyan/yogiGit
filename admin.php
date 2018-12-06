82c6189f72b378fb97dee95f75b2960ac21b5af4
khanaly <khanaly@ndsl-scss-2270.fios-router.home>
2018-12-06 15:43:24 -0500

    implemented the export functionality and print

--- admin.php
+++ admin.php
@@ -2,16 +2,24 @@
 <html lang="en">
 <head>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css">
+
 <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
 <script>
     $( document ).ready(function() {
     	 $('#cnrm_date').DataTable( {
-    	        //"processing": true,
-    	       // "serverSide": true
-    	        //,"ajax": "../server_side/scripts/server_processing.php"
-    		 	//"aaData" : {d,e,f},
-		 		//"aoColumns" : {a,b,c}
+    	        dom: 'Bfrtip',
+    	        buttons: [
+    	            'copy', 'csv', 'excel', 'pdf', 'print'
+    	        ]
     	    } );
     });
  

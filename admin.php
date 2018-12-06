d6a46152d5470b01c1156620a1e6f49dcd3bff74
khanaly <khanaly@ndsl-scss-2270.fios-router.home>
2018-12-06 14:21:59 -0500

    page to implete data table

--- admin.php
+++ admin.php
@@ -1,3 +1,31 @@
-<?php
-echo "Admin Page";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+</head>
+
+
+
+<div class="">
+		<table id="cnrm_date" class="display" width="100%" cellspacing="0">
+        <thead>
+            <tr>
+                <th>subject_id</th>
+                <th>first_name</th>
+				<th>phone_number</th>               
+            </tr>
+        </thead>
+ 
+        <tfoot>
+            <tr>
+               <th>subject_id</th>
+                <th>first_name</th>
+				<th>phone_number</th>               
+            </tr>
+        </tfoot>
+    </table>
+    </div>
+
+</html>

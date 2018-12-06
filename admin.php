d79c81fb31d725e5d48cae27aee33de50d573fda
khanaly <khanaly@ndsl-scss-2270.fios-router.home>
2018-12-06 15:14:55 -0500

    simplest hard coded data table

--- admin.php
+++ admin.php
@@ -4,28 +4,61 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script>
+    $( document ).ready(function() {
+    	 $('#cnrm_date').DataTable( {
+    	        //"processing": true,
+    	       // "serverSide": true
+    	        //,"ajax": "../server_side/scripts/server_processing.php"
+    		 	//"aaData" : {d,e,f},
+		 		//"aoColumns" : {a,b,c}
+    	    } );
+    });
+ 
+ 
+    </script>
 </head>
 
 
 
-<div class="">
-		<table id="cnrm_date" class="display" width="100%" cellspacing="0">
+<table id="cnrm_date" class="display" style="width:100%">
         <thead>
             <tr>
-                <th>subject_id</th>
-                <th>first_name</th>
-				<th>phone_number</th>               
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
             </tr>
         </thead>
- 
-        <tfoot>
+        <tbody>
             <tr>
-               <th>subject_id</th>
-                <th>first_name</th>
-				<th>phone_number</th>               
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td>Garrett Winters</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>63</td>
+                <td>2011/07/25</td>
+                <td>$170,750</td>
+            </tr>
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
             </tr>
         </tfoot>
-    </table>
-    </div>
 
 </html>

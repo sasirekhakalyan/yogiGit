0b597ad41005b4849525491c8eca10db314e4aa2
khanaly <khanaly@ndsl-scss-2270.fios-router.home>
2018-12-06 19:16:04 -0500

    this will work very soon.may be tomorrow.

--- admin.php
+++ admin.php
@@ -1,9 +1,11 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css">
 
+<!-- <script type="text/javascript"  src="js/cnrm.js"/> -->
 <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
@@ -14,59 +16,46 @@
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
 <script>
-    $( document ).ready(function() {
-    	 $('#cnrm_date').DataTable( {
-    	        dom: 'Bfrtip',
+$( document ).ready(function() {
+$.ajax({url: "report.php", 
+	dataType: "json",
+	method: "GET",
+	success: function(result){	
+        	$('#cnrm_data').dataTable( {
+        		data: result,
+        		dom: 'Bfrtip',
     	        buttons: [
     	            'copy', 'csv', 'excel', 'pdf', 'print'
     	        ]
-    	    } );
-    });
- 
- 
-    </script>
+        	 } );
+    }});
+
+});
+
+</script>
+
 </head>
 
 
-
-<table id="cnrm_date" class="display" style="width:100%">
+<table id="cnrm_data" class="display" style="width:100%">
         <thead>
             <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
+            	<th>subject_id</th>
+                <th>first_name</th>
+                <th>middle_name</th>
+                <th>last_name</th>              
             </tr>
         </thead>
-        <tbody>
+        <tfoot>
             <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
-            </tr>
-            </tbody>
-            <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
+               	<th>subject_id</th>
+                <th>first_name</th>
+                <th>middle_name</th>
+                <th>last_name</th>
             </tr>
         </tfoot>
-
+        </table>
+       
 </html>
+
+
--- report.php
+++ report.php
@@ -1,20 +1,21 @@
 <?php
 require_once 'pg_connection.php';
-// DB table to use
-$table = 'datatables_demo';
-
-// Table's primary key
-$primaryKey = 'id';
-$result = pg_query($db,"SELECT first_name,last_name FROM cnrm_clinical_trial_referral");
-
+$result = pg_query($db,"SELECT subject_id,first_name,middle_name,last_name FROM cnrm_clinical_trial_referral");
+$data = array();   
 while($row = pg_fetch_assoc($result))
 {
-    $data[] = $row;
+    $data[] = array(
+        "subject_id" => $row["subject_id"],
+        "first_name" => $row["first_name"],
+        "middle_name" => $row["middle_name"],
+        "last_name" => $row["last_name"]
+       
+    );
 }
-
-if(isset($data))
-{
+//if(isset($data))
+//{
     header('Content-Type: application/json');
-    echo json_encode($data); 
-}
+    //echo json_encode($data); 
+    echo json_encode(array("data" => $data));
+//}
 ?>
